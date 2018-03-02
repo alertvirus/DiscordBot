401a495b32eca044fa6e5a108b2308052b7e31c6
Azam Hussain <azamhussain.53@gmail.com>
2018-03-02 11:38:12 +0530

    Add project files.

---
+++ TestBot/Modules/Misc.cs
@@ -0,0 +1,43 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBot.Modules
+{
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+        [Command("Echo")]
+        public async Task Echo([Remainder]string message)
+        {
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Echoed Message");
+            embed.WithDescription(message);
+            embed.WithColor(new Color(0, 255, 0));
+
+            await Context.Channel.SendMessageAsync("", false,embed);
+        }
+
+        [Command("txtdwnld")]
+        public async Task TextDownload()
+        {
+            var messages = await this.Context.Channel.GetMessagesAsync(10).Flatten();
+
+            var list = new List<string>();
+
+            foreach(var text in messages)
+            {
+                list.Add(text.Content);    
+            }
+            string test = string.Join(",", list.ToArray());
+            Console.WriteLine(test);
+
+
+
+
+        }
+    }
+}
